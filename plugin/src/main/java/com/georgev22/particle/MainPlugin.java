8af2b527f665c3b12d3da53ab7a1baa296f75758
George V. <georgev22.github@gmail.com>
2025-12-14 08:22:39 +0200

    refactor: restructure project into multi-module setup
    This change reorganizes the project from a single module to a multi-module
    structure with separate 'plugin' and 'lib' modules. The existing particle
    library code has been moved to the 'lib' module, while a new Bukkit plugin
    module has been created with proper build configuration and plugin entry
    point.
    
    The settings.gradle file now includes both modules, and build.gradle files
    have been separated accordingly. This separation allows for cleaner
    dependency management and enables the library to be used independently
    of the plugin implementation.

---
+++ plugin/src/main/java/com/georgev22/particle/MainPlugin.java
@@ -0,0 +1,17 @@
+package com.georgev22.particle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
